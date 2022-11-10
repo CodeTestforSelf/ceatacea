919a5b0adb2a183ba02ec42858233b4cc77d326a
syoh5188 <syoh5188@gmail.com>
2022-11-10 20:49:40 +0900

    2022-11-10

--- auto_input_testing.py
+++ auto_input_testing.py
@@ -5,14 +5,18 @@ import sys
 import re
 import datetime
 
-N = int(input())
+input = sys.stdin.readline()
+# 그리디
+def ch11_01():
+    N = int(input) # (1 <= N <= 100,000) -> O(N) 가능
+    X = sorted(map(int, input.split()))
+    count, group = 0, 0
+    for n in X:
+        # count += 1 # 모험가 수
+        if n <= N:
+            group += 1 # 그룹 수
+            count = 0 # 그룹 결성 되었으므로 초기화
+    return group
 
-count = 0
 
-coin_types = [500, 100, 50, 10]
-
-for coin in coin_types:
-    count += N // coin
-    N %= coin
-
-print(count)
+print(ch11_01())
