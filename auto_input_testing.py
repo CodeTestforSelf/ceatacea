85da7c75d6e470e59b225db70374d24360a9f55d
syoh5188 <syoh5188@gmail.com>
2022-11-14 18:53:41 +0900

    2022-11-14

--- auto_input_testing.py
+++ auto_input_testing.py
@@ -8,7 +8,6 @@ import datetime
 # input = sys.stdin.readline()
 
 # 제출코드 :
-import math
 
 def bj_1026():
     N = int(input())
@@ -16,8 +15,8 @@ def bj_1026():
     B = sorted(map(int, input().split()),reverse=False)
     result = 0
     for a,b in zip(A,B):
-        prod = math.prod([a,b])
-        result += prod
+        # prod = math.prod([a,b])
+        result += (a*b)# prod
     return result
 
 print(bj_1026())
