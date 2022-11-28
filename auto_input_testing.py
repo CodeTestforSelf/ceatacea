a9a2e8bb128827ad8ad39c9c7ff587251d86c829
syoh5188 <syoh5188@gmail.com>
2022-11-28 16:02:14 +0900

    afternoon update

--- auto_input_testing.py
+++ auto_input_testing.py
@@ -8,18 +8,24 @@ import datetime
 # input = sys.stdin.readline()
 
 # 제출코드 :
+def fun4_3 () :
+	# input -> a1 일 때, col = a, row 1
+	# ord(str) -> 유니코드 정수를 반환
+	# a : 97 ~ h : 104 -> 8 * 8 안에서 해야 하므로 97에서 빼고 +1 해서 1~8 만들어야 함
+	position = input()
+	col = int(ord(position[0])) - int(ord('a')) + 1
+	row = int(position[1]) # str -> int
+	# 경우의 수로서 가능성들
+	moves = [(-2, -1), (-2, 1), (2, -1), (2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2)]
+	count = 0 # 경우의 수 카운팅
+	for m in moves:
+		cols = col + int(m[0])
+		rows = row + int(m[1])
+		if (1 <= cols <= 8) and (1 <= rows <= 8):
+		# 범위 벗어나지 않을 때만
+			count += 1
 
-def bj_2839():
-    sugar = int(input())
-    bags = [5, 3]
-    result = 0
-    for b in bags:
-        print(f'start -- b : {b} / result : {result} / sugar : {sugar}')
-        result += sugar // b
-        print(f'result += sugar // b --> b : {b} / result : {result} / sugar : {sugar}')
-        sugar %= b
-        print(f'sugar %= b --> b : {b} / result : {result} / sugar : {sugar}')
+	return count
 
-    return -1 if sugar != 0 else result
 
-print(bj_2839())
+print(fun4_3())
